175e0747daa7c9137db0673ae5274b180ebfd5c4
Albert Wang <albertyw@mit.edu>
2017-01-09 17:13:46 +0900

    Add FocusObject and GithubHost classes and tests

--- browse.py
+++ browse.py
@@ -2,12 +2,53 @@
 
 
 class GithubHost(object):
+    GITHUB_URL = "https://github.com/"
+
     def __init__(self, user, repository):
         self.user = user
         self.repository = repository
 
     def get_url(self, focus_object):
-        pass
+        repository_url = "%s%s/%s" % (
+            self.GITHUB_URL,
+            self.user,
+            self.repository
+        )
+        if focus_object.is_root:
+            return self.root_url(repository_url, focus_object)
+        if focus_object.is_directory:
+            return self.directory_url(repository_url, focus_object)
+        return self.file_url(repository_url, focus_object)
+
+    def root_url(self, repository_url, focus_object):
+        return repository_url
+
+    def directory_url(self, repository_url, focus_object):
+        repository_url = "%s/tree/master%s" % (
+            repository_url,
+            focus_object.path
+        )
+        return repository_url
+
+    def file_url(self, repository_url, focus_object):
+        repository_url = "%s/blob/master%s" % (
+            repository_url,
+            focus_object.path
+        )
+        return repository_url
+
+
+class FocusObject(object):
+    def __init__(self, path):
+        self.path = path
+
+    @property
+    def is_root(self):
+        return self.path == '/'
+
+    @property
+    def is_directory(self):
+        return self.path[-1] == '/'
 
 
 def get_repository_host():
--- test.py
+++ test.py
@@ -4,13 +4,64 @@ import browse
 
 
 class TestGithubHost(unittest.TestCase):
+    def setUp(self):
+        self.github_host = browse.GithubHost('albertyw', 'git-browse')
+        self.repository_url = 'https://github.com/albertyw/git-browse'
+        self.focus_object = browse.FocusObject('/')
+
     def test_init(self):
         host = browse.GithubHost('user', 'repository')
         self.assertEqual(host.user, 'user')
         self.assertEqual(host.repository, 'repository')
 
     def test_get_url(self):
-        pass
+        url = self.github_host.get_url(self.focus_object)
+        self.assertEqual(url, self.repository_url)
+
+    def test_root_url(self):
+        url = self.github_host.root_url(self.repository_url, self.focus_object)
+        self.assertEqual(url, self.repository_url)
+
+    def test_directory_url(self):
+        self.focus_object.path = '/asdf/'
+        url = self.github_host.directory_url(
+            self.repository_url,
+            self.focus_object
+        )
+        self.assertEqual(
+            url,
+            'https://github.com/albertyw/git-browse/tree/master/asdf/'
+        )
+
+    def test_file_url(self):
+        self.focus_object.path = '/README.md'
+        url = self.github_host.file_url(self.repository_url, self.focus_object)
+        self.assertEqual(
+            url,
+            'https://github.com/albertyw/git-browse/blob/master/README.md'
+        )
+
+
+class FocusObject(unittest.TestCase):
+    def test_init(self):
+        obj = browse.FocusObject('/asdf')
+        self.assertEqual(obj.path, '/asdf')
+
+    def test_is_root(self):
+        obj = browse.FocusObject('/')
+        self.assertTrue(obj.is_root)
+
+    def test_is_not_root(self):
+        obj = browse.FocusObject('/asdf/')
+        self.assertFalse(obj.is_root)
+
+    def test_is_directory(self):
+        obj = browse.FocusObject('/asdf/')
+        self.assertTrue(obj.is_directory)
+
+    def test_is_not_directory(self):
+        obj = browse.FocusObject('/asdf')
+        self.assertFalse(obj.is_directory)
 
 
 class TestGetRepositoryHost(unittest.TestCase):
