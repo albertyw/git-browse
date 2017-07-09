249660eebd08176373893a4aa8f324e02ca17574
Albert Wang <git@albertyw.com>
2017-07-09 12:18:23 -0700

    Add instructions for installing python 3

--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@
 # Installs this repository so that you can run `git browse` from anywhere in
 # your filesystem
 
+# Check if python 3 is installed
+if ! command -v python3 > /dev/null 2>&1; then
+    echo "You must install python 3 to use git browse"
+    echo "On OSX, use 'brew install python3'"
+    echo "On Ubuntu/Debian, use 'sudo apt install python3'"
+fi
+
 REPOSITORY_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 git config --global \
