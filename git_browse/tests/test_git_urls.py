c389b23a4a1e061eff2c4f1d96da95b8dbf3a161
Albert Wang <git@albertyw.com>
2021-01-12 23:27:53 -0800

    Update tests

--- git_browse/tests/test_git_urls.py
+++ git_browse/tests/test_git_urls.py
@@ -1,5 +1,6 @@
 import os
-from typing import Callable, List, NamedTuple, Union, cast
+import subprocess
+from typing import Callable, List, NamedTuple, Optional, cast
 import unittest
 from unittest.mock import MagicMock, patch
 
@@ -10,7 +11,8 @@ from git_browse.tests import test_util
 class TestConfig(NamedTuple):
     git_url: str
     target_path: str
-    host_url: Union[str, List[str]]
+    host_url: Optional[str]
+    subprocess_command: Optional[List[str]]
 
 
 directory = os.path.dirname(os.path.realpath(__file__))
@@ -22,76 +24,91 @@ GIT_URLS: List[TestConfig] = [
         'git@github.com:albertyw/git-browse',
         '',
         'https://github.com/albertyw/git-browse',
+        None,
     ),
     TestConfig(
         'git@github.com:albertyw/git-browse.git',
         '',
         'https://github.com/albertyw/git-browse',
+        None,
     ),
     TestConfig(
         'https://github.com/albertyw/git-browse.git',
         '',
         'https://github.com/albertyw/git-browse',
+        None,
     ),
     TestConfig(
         'https://github.com/albertyw/git-browse',
         '',
         'https://github.com/albertyw/git-browse',
+        None,
     ),
     TestConfig(
         'git@github.com:albertyw/git-browse',
         'README.md',
         'https://github.com/albertyw/git-browse/blob/master/README.md',
+        None,
     ),
     TestConfig(
         'git@github.com:albertyw/git-browse',
         TEST_DIR,
         'https://github.com/albertyw/git-browse/tree/master/testdir/',
+        None,
     ),
     TestConfig(
         'git@github.com:albertyw/git-browse',
         test_util.get_tag(),
         'https://github.com/albertyw/git-browse/commit/' +
         test_util.get_tag_commit_hash(),
+        None,
     ),
     TestConfig(
         'gitolite@code.uber.internal:a/b',
         '',
+        None,
         ['arc', 'browse', '.'],
     ),
     TestConfig(
         'gitolite@config.uber.internal:a/b',
         '',
+        None,
         ['arc', 'browse', '.'],
     ),
     TestConfig(
         'gitolite@code.uber.internal:a/b',
         'README.md',
+        None,
         ['arc', 'browse', 'README.md'],
     ),
     TestConfig(
         'gitolite@code.uber.internal:a/b',
         TEST_DIR,
+        None,
         ['arc', 'browse', TEST_DIR+'/'],
     ),
     TestConfig(
         'gitolite@code.uber.internal:a/b',
         test_util.get_tag(),
+        None,
         ['arc', 'browse', test_util.get_tag_commit_hash()],
     ),
     TestConfig(
         'gitolite@code.uber.internal:a/b',
         'D123',
+        None,
         ['arc', 'browse', 'D123'],
     ),
     TestConfig(
         'gitolite@code.uber.internal:a',
         'README.md',
+        None,
         ['arc', 'browse', 'README.md'],
     ),
     TestConfig(
         'https://code.uber.internal/x/y',
         'README.md',
+        None,
         ['arc', 'browse', 'README.md'],
     ),
 ]
@@ -100,6 +117,8 @@ GIT_URLS: List[TestConfig] = [
 class TestGitURLs(unittest.TestCase):
     def setUp(self) -> None:
         os.mkdir(TEST_DIR_PATH)
+        self.mock_popen_patcher = patch('subprocess.Popen')
+        self.addCleanup(self.mock_popen_patcher.stop)
 
     def tearDown(self) -> None:
         os.rmdir(TEST_DIR_PATH)
@@ -107,14 +126,41 @@ class TestGitURLs(unittest.TestCase):
 
 def generate_test(test_config: TestConfig) -> Callable[[], None]:
     @patch('git_browse.browse.get_git_url')
-    def test(self: TestGitURLs, mock_get_git_url: MagicMock) -> None:
+    def test(
+        self: TestGitURLs,
+        mock_get_git_url: MagicMock
+    ) -> None:
+
         mock_get_git_url.return_value = test_config.git_url
         host = browse.get_repository_host()
         focus_object = browse.get_git_object(
             test_config.target_path, REPO_PATH, host
         )
+        self.mock_popen_patcher.start()
+        mock_popen = cast(MagicMock, subprocess.Popen)
+
+        if test_config.subprocess_command:
+            self.assertEqual(test_config.host_url, None)
+            return_data = 'asdf\nurl'
+            mock_popen().returncode = 0
+            mock_popen().communicate.return_value = (return_data, '')
+            host_url = 'url'
+        else:
+            self.assertEqual(test_config.subprocess_command, None)
+            assert test_config.host_url is not None
+            host_url = test_config.host_url
+
         url = host.get_url(focus_object)
-        self.assertEqual(test_config.host_url, url)
+        self.assertEqual(host_url, url)
+
+        if test_config.subprocess_command:
+            found = False
+            for call in mock_popen.call_args_list:
+                if call.args and call.args[0] == \
+                        test_config.subprocess_command:
+                    found = True
+                    break
+            self.assertTrue(found)
     test = cast(Callable[[], None], test)
     return test
 
