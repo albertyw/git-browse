a29d7e5618984fcf529c094f619cc991cb129cb9
Albert Wang <git@albertyw.com>
2021-07-14 00:32:44 -0700

    Add BitbucketHost

--- git_browse/browse.py
+++ git_browse/browse.py
@@ -145,6 +145,74 @@ class GithubHost(Host):
         return None
 
 
+class BitbucketHost(Host):
+    BITBUCKET_URL = "https://bitbucket.org/"
+    user: str = ''
+    repository: str = ''
+
+    def __init__(self, user: str, repository: str) -> None:
+        self.user = user
+        self.repository = repository
+
+    @staticmethod
+    def create(url_regex_match: Match[str]) -> 'Host':
+        repository = url_regex_match.group('repository')
+        if repository[-4:] == '.git':
+            repository = repository[:-4]
+        user = url_regex_match.group('user')
+        return BitbucketHost(user, repository)
+
+    def set_host_class(self, host_class: Type[Host]) -> None:
+        return
+
+    def get_url(self, git_object: 'GitObject') -> str:
+        repository_url = "%s%s/%s" % (
+            self.BITBUCKET_URL,
+            self.user,
+            self.repository
+        )
+        if git_object.is_commit_hash():
+            return self.commit_hash_url(repository_url, git_object)
+        if git_object.is_root():
+            return self.root_url(repository_url, git_object)
+        if git_object.is_directory():
+            return self.directory_url(repository_url, git_object)
+        return self.file_url(repository_url, git_object)
+
+    def commit_hash_url(
+            self,
+            repository_url: str,
+            focus_hash: 'GitObject') -> str:
+        repository_url = "%s/commits/%s" % (
+            repository_url,
+            focus_hash.identifier
+        )
+        return repository_url
+
+    def root_url(self, repository_url: str, focus_object: 'GitObject') -> str:
+        return repository_url
+
+    def directory_url(
+            self,
+            repository_url: str,
+            focus_object: 'GitObject') -> str:
+        repository_url = "%s/src/master/%s" % (
+            repository_url,
+            focus_object.identifier
+        )
+        return repository_url
+
+    def file_url(self, repository_url: str, focus_object: 'GitObject') -> str:
+        repository_url = "%s/src/master/%s" % (
+            repository_url,
+            focus_object.identifier
+        )
+        return repository_url
+
+    def valid_focus_object(self, arg: str) -> Optional['GitObject']:
+        return None
+
+
 class PhabricatorHost(Host):
     PHABRICATOR_OBJECT_REGEX = '^[DT][0-9]+$'
     user: str = ''
