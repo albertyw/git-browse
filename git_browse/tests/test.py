cae6e411d6978cc93a3983681559b655e8e0e918
Albert Wang <git@albertyw.com>
2021-07-14 00:32:48 -0700

    Add tests

--- git_browse/tests/test.py
+++ git_browse/tests/test.py
@@ -65,6 +65,57 @@ class TestGithubHost(unittest.TestCase):
         )
 
 
+class TestBitbucketHost(unittest.TestCase):
+    def setUp(self) -> None:
+        self.github_host = browse.BitbucketHost('albertyw', 'git-browse')
+        self.repository_url = 'https://bitbucket.org/albertyw/git-browse'
+        self.focus_object = browse.FocusObject('/')
+        self.focus_hash = browse.FocusHash(test_util.get_tag())
+
+    def test_init(self) -> None:
+        host = browse.BitbucketHost('user', 'repository')
+        self.assertEqual(host.user, 'user')
+        self.assertEqual(host.repository, 'repository')
+
+    def test_get_url(self) -> None:
+        url = self.github_host.get_url(self.focus_object)
+        self.assertEqual(url, self.repository_url)
+
+    def test_root_url(self) -> None:
+        url = self.github_host.root_url(self.repository_url, self.focus_object)
+        self.assertEqual(url, self.repository_url)
+
+    def test_directory_url(self) -> None:
+        self.focus_object.identifier = 'asdf/'
+        url = self.github_host.directory_url(
+            self.repository_url,
+            self.focus_object
+        )
+        self.assertEqual(
+            url,
+            'https://bitbucket.org/albertyw/git-browse/src/master/asdf/'
+        )
+
+    def test_file_url(self) -> None:
+        self.focus_object.identifier = 'README.md'
+        url = self.github_host.file_url(self.repository_url, self.focus_object)
+        self.assertEqual(
+            url,
+            'https://bitbucket.org/albertyw/git-browse/src/master/README.md'
+        )
+
+    def test_commit_hash_url(self) -> None:
+        url = self.github_host.commit_hash_url(
+            self.repository_url,
+            self.focus_hash
+        )
+        self.assertEqual(
+            url,
+            'https://bitbucket.org/albertyw/git-browse/commits/%s'
+            % test_util.get_tag()
+        )
+
+
 class SourcegraphHost(unittest.TestCase):
     def setUp(self) -> None:
         self.obj = browse.SourcegraphHost('code.uber.internal', 'asdf')
