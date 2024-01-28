d24e9b1d45e04f7fc8e4a2356b68828bb450f745
Albert Wang <git@albertyw.com>
2024-01-28 00:28:19 -0800

    Namespace all sg.uberinternal.com tests under uber

--- git_browse/tests/test_sourcegraph.py
+++ git_browse/tests/test_sourcegraph.py
@@ -9,59 +9,59 @@ BASE_DIRECTORY = pathlib.Path(__file__).parents[2]
 
 class SourcegraphHost(unittest.TestCase):
     def setUp(self) -> None:
-        self.obj = sourcegraph.SourcegraphHost(
+        self.uber_obj = sourcegraph.SourcegraphHost(
             typedefs.GitConfig("", "master"),
             "code.uber.internal",
             "asdf",
         )
-        self.obj.host_class = phabricator.PhabricatorHost
+        self.uber_obj.host_class = phabricator.PhabricatorHost
 
-    def test_init(self) -> None:
-        self.assertEqual(self.obj.host, "code.uber.internal")
-        self.assertEqual(self.obj.repository, "asdf")
+    def test_uber_init(self) -> None:
+        self.assertEqual(self.uber_obj.host, "code.uber.internal")
+        self.assertEqual(self.uber_obj.repository, "asdf")
 
-    def test_create(self) -> None:
+    def test_uber_create(self) -> None:
         repo = "gitolite@code.uber.internal:a/b"
         git_config = typedefs.GitConfig(repo, "master")
         git_config.try_url_match(phabricator.UBER_SSH_GITOLITE_URL)
         obj = sourcegraph.SourcegraphHost.create(git_config)
         self.assertEqual(obj.repository, "a/b")
 
-    def test_create_dot_git(self) -> None:
+    def test_uber_create_dot_git(self) -> None:
         repo = "gitolite@code.uber.internal:a/b.git"
         git_config = typedefs.GitConfig(repo, "master")
         git_config.try_url_match(phabricator.UBER_SSH_GITOLITE_URL)
         obj = sourcegraph.SourcegraphHost.create(git_config)
         self.assertEqual(obj.repository, "a/b")
 
-    def test_get_url_commit(self) -> None:
+    def test_uber_get_url_commit(self) -> None:
         git_object = typedefs.FocusHash("abcd")
-        url = self.obj.get_url(git_object)
+        url = self.uber_obj.get_url(git_object)
         self.assertEqual(
             url,
             sourcegraph.UBER_SOURCEGRAPH_URL
             + "code.uber.internal/uber-code/asdf/-/commit/abcd",
         )
 
-    def test_get_url_root(self) -> None:
+    def test_uber_get_url_root(self) -> None:
         git_object = typedefs.FocusObject(os.sep)
-        url = self.obj.get_url(git_object)
+        url = self.uber_obj.get_url(git_object)
         self.assertEqual(
             url, sourcegraph.UBER_SOURCEGRAPH_URL + "code.uber.internal/uber-code/asdf"
         )
 
-    def test_get_url_directory(self) -> None:
+    def test_uber_get_url_directory(self) -> None:
         git_object = typedefs.FocusObject("zxcv" + os.sep)
-        url = self.obj.get_url(git_object)
+        url = self.uber_obj.get_url(git_object)
         self.assertEqual(
             url,
             sourcegraph.UBER_SOURCEGRAPH_URL
             + "code.uber.internal/uber-code/asdf/-/tree/zxcv/",
         )
 
-    def test_get_url_file(self) -> None:
+    def test_uber_get_url_file(self) -> None:
         git_object = typedefs.FocusObject("zxcv")
-        url = self.obj.get_url(git_object)
+        url = self.uber_obj.get_url(git_object)
         self.assertEqual(
             url,
             sourcegraph.UBER_SOURCEGRAPH_URL
